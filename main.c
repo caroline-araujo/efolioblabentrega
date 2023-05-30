97e01b8239dbc88e27591762e575f0f096804284
Caroline Araujo <carolinearaujo570@gmail.com>
2023-05-30 20:09:37 +0100

    ajustando o menu

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+#include <string.h>
 #include "structs.h"
 #include "createrectangle.h"
 #include "move.h"
@@ -7,22 +9,35 @@
 #include "display.h"
 
 int main() {
+
+    setlocale(LC_ALL, "Portuguese");
+
     Rectangle rectangles[MAX_RECTANGLES];
     int numRectangles = 0;
 
     int command;
-    int x, y, l, h, p;
+    int x, y, l, h, p, op;
 
-    while (1) {
-        printf("Escolha o comando:\n");
-        printf("1 - create\n");
-        printf("2 - moveright\n");
-        printf("3 - moveleft\n");
-        printf("Comando: ");
-        scanf("%d", &command);
+    printf ("\t\t\t\t======================= MUNDO DOS RETANGULOS =======================\n\n");
 
-        switch (command) {
-            case 1:
+    do {
+        printf("\n\tMENU\n\n");
+        printf ("1 - Editar mundo \n");
+        printf ("0 - Sair\n");
+        scanf ("%d", &op);
+
+        switch (op) {
+
+        case 1:
+            printf("\nDigite o comando:\n\n");
+            printf("1 - create\n");
+            printf("2 - moveright\n");
+            printf("3 - moveleft\n\n");
+            printf("Comando: ");
+            char command[20];
+            scanf("%s", command);
+
+            if (strcmp(command, "create") == 0) {
                 printf("Digite as coordenadas: \n");
                 printf("Digite a coordenada x: ");
                 scanf("%d", &x);
@@ -33,9 +48,7 @@ int main() {
                 printf("Digite a altura h: ");
                 scanf("%d", &h);
                 createRectangle(rectangles, &numRectangles, x, y, l, h);
-                break;
-
-            case 2:
+            } else if (strcmp(command, "moveright") == 0) {
                 printf("Digite as coordenadas: \n");
                 printf("Digite a coordenada x: ");
                 scanf("%d", &x);
@@ -44,9 +57,7 @@ int main() {
                 printf("Digite a posição p: ");
                 scanf("%d", &p);
                 moveRight(rectangles, numRectangles, x, y, p);
-                break;
-
-            case 3:
+            } else if (strcmp(command, "moveleft") == 0) {
                 printf("Digite as coordenadas: \n");
                 printf("Digite a coordenada x: ");
                 scanf("%d", &x);
@@ -55,30 +66,37 @@ int main() {
                 printf("Digite a posição p: ");
                 scanf("%d", &p);
                 moveLeft(rectangles, numRectangles, x, y, p);
-                break;
-
-            default:
+            } else {
                 printf("Comando inválido!\n");
+            }
+
+                // Exibir retângulos no console
+                displayRectangles(rectangles, numRectangles);
+
+                // Calcular o grau de instabilidade
+                float instabilityDegree = calculateInstability(rectangles, numRectangles);
+
+                // Exibir o grau de instabilidade
+                printf("Grau de instabilidade: %.2f\n", instabilityDegree);
+
+                // Verificar se o grau de instabilidade é superior ou igual a 70%
+                if (instabilityDegree >= 0.7) {
+                    printf("ALERTA: Grau de instabilidade superior ou igual a 70%!\n");
+                }
+
+                // Limpar o buffer de entrada
+                while (getchar() != '\n');
                 break;
-        }
 
-        // Exibir retângulos no console
-        displayRectangles(rectangles, numRectangles);
+            case 2:
+                return 0;
+                break;
 
-        // Calcular o grau de instabilidade
-        float instabilityDegree = calculateInstability(rectangles, numRectangles);
-
-        // Exibir o grau de instabilidade
-        printf("Grau de instabilidade: %.2f\n", instabilityDegree);
-
-        // Verificar se o grau de instabilidade é superior ou igual a 70%
-        if (instabilityDegree >= 0.7) {
-            printf("ALERTA: Grau de instabilidade superior ou igual a 70%!\n");
-        }
-
-        // Limpar o buffer de entrada
-        while (getchar() != '\n');
-    }
+            default: 
+                printf ("Opção inválida");
+                break;
+            }
+        } while (op == 1);
 
     return 0;
 }
